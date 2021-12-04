aa4d5438802b15d37dacc2f609875ce38dccabc1
Nimesh Jethwa <nimesh_jethwa@trendmicro.com>
2021-12-04 00:00:30 -0500

    Read file as string and optionally convert to i64

--- aoc2021/src/bin/day1.rs
+++ aoc2021/src/bin/day1.rs
@@ -1,8 +1,9 @@
 use common::{
+  convert_vector_string_to_i64,
   read_file
 };
 
-fn part1_count(numbers: &[i64]){
+fn p1(numbers: &Vec<i64>){
     let mut count = 0;
     for x in 0..numbers.len()-2 {
       if numbers[x+1] > numbers[x] {
@@ -13,7 +14,7 @@ fn part1_count(numbers: &[i64]){
     println!("Part1 count: {}", count)
 }
 
-fn part2_count(numbers: &mut Vec<i64>){
+fn p2(numbers: &mut Vec<i64>){
   // Creating another vector with 3 zeros to append to original vector. This new vector needs to be mutable as well in order to be appended.
   let mut filler: Vec<i64> = vec![0; 3];
   numbers.append(&mut filler);
@@ -30,7 +31,7 @@ fn part2_count(numbers: &mut Vec<i64>){
 }
 
 fn main() {
-    let mut items: Vec<i64> = read_file("aoc2021/files/day1-input.txt");
-    part1_count(&items); // pass vector as is.
-    part2_count(&mut items); // pass mutable vector as elements will be added to it.
+    let mut items: Vec<i64> = convert_vector_string_to_i64(read_file("aoc2021/files/day1-input.txt"));
+    p1(&items); // pass vector as is.
+    p2(&mut items); // pass mutable vector as elements will be added to it.
 }
--- common/src/lib.rs
+++ common/src/lib.rs
@@ -5,20 +5,24 @@ use project_root;
 use std::path::Path;
 use std::path::PathBuf;
 
-pub fn read_file(input_file: &str) -> Vec<i64> {
+pub fn read_file(input_file: &str) -> Vec<String> {
   read_file_return_vec(&get_input_file_pathbuf(&input_file))
+}
+
+pub fn convert_vector_string_to_i64(v: Vec<String>) -> Vec<i64> {
+  v.iter().flat_map(|x| x.parse()).collect()
 }
 
 pub fn get_input_file_pathbuf(input_file: &str) -> PathBuf {
   [project_root::get_project_root().unwrap(), Path::new(input_file).to_path_buf()].iter().collect() // construct a path to input file
 }
 
-pub fn read_file_return_vec(file_path: &PathBuf) -> Vec<i64> {
+pub fn read_file_return_vec(file_path: &PathBuf) -> Vec<String> {
     let file = File::open(file_path).expect("File not found");
     let reader = BufReader::new(file);
 
     reader
         .lines()
-        .map(|line| line.unwrap().parse::<i64>().unwrap())
+        .map(|line| line.unwrap().parse().unwrap())
         .collect()
 }
